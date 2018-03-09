f49be2148a5e90f8064603f40711dc4383da3253
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-09 13:16:00 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,9 +1,11 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 
 def q01_load_data(path):
-    "write your solution here"
-   
+    'write your solution here'
+    data = pd.read_csv(path, encoding='utf-8')
+    return data
 
