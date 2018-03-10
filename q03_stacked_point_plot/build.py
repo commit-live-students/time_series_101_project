7f9f758fc3b5543e7498dc531eade37a9cf72797
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-03-10 05:08:01 +0000

    Done

--- q03_stacked_point_plot/build.py
+++ q03_stacked_point_plot/build.py
@@ -1,12 +1,13 @@
 
 import pandas as pd
 import numpy as np
-plt.switch_backend('agg')
 #import sys
 #sys.path.append('./')
 from sklearn.model_selection import train_test_split
 from greyatomlib.time_series_101_project.q02_data_splitter.build import q02_data_splitter
 import seaborn as sns
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 def q03_stacked_point_plot(path, x_column_name='month', y_column_name='Sales', hue='year',
         order_of_the_axis=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec']):
