95c6eacc83d8f4c3242150fe1a0efadace2403d9
sonal7930 <sonal.chandra7930@gmail.com>
2018-05-04 11:40:13 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -5,8 +5,11 @@ import math
 #import sys
 #sys.path.append('./')
 from greyatomlib.time_series_101_project.q01_load_data.build import q01_load_data
-
+path = 'data/perrin-freres-monthly-champagne.csv'
 
 def q02_data_splitter(path):
-    data = q01_load_data(path)	
-    '''Write your solution here'''
+    data = q01_load_data(path)
+    data["Month"] = pd.to_datetime(data["Month"])
+    X_train = data[data["Month"] < '1971-10-01']
+    X_valid = data[data["Month"] >= '1971-10-01']
+    return X_train,X_valid
