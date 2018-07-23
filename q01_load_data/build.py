28764e079c2360b3c763bee46549589145af58a4
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-23 18:07:33 +0530

    Update build.py

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-
-def q01_load_data(path):
-    "write your solution here"
+"write your solution here"
    
 
