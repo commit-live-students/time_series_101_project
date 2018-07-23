49b4eeab36d654af8dbfd892a98cef552cdde884
bhushan45 <bhushan45@users.noreply.github.com>
2018-07-23 18:10:22 +0530

    Update build.py

--- q04_boxplot/build.py
+++ q04_boxplot/build.py
@@ -9,9 +9,6 @@ import datetime
 # sys.path.append('./')
 from greyatomlib.time_series_101_project.q02_data_splitter.build import q02_data_splitter
 
-def q04_boxplot(path, x="month", y="Sales", kind="box", order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
-       'Sep', 'Oct', 'Nov', 'Dec'], size=8):
-    train, validation = q02_data_splitter(path)
-    "write your solution here"
+"write your solution here"
     
 
