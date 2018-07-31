ecb468cef0ff8f6c2c22300b1c347a9c4e7841c4
Vagus30 <vishwas.tejaswi@gmail.com>
2018-07-31 20:48:54 +0000

    Done

--- q04_boxplot/build.py
+++ q04_boxplot/build.py
@@ -1,3 +1,4 @@
+# %load q04_boxplot/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,6 +10,16 @@ import datetime
 # sys.path.append('./')
 from greyatomlib.time_series_101_project.q02_data_splitter.build import q02_data_splitter
 
-"write your solution here"
+'write your solution here'
+def q04_boxplot(path,x='month',y='Sales',kind='box',order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec'],size=8):
     
+    x_train,x_valid = q02_data_splitter(path)
+    x_train['month'] = x_train['Month'].dt.strftime('%b')
+    x_train['year'] = x_train['Month'].dt.year
+    plt.figure(figsize=(16,7))
+    sns.factorplot(x= x,y=y,data=x_train,kind=kind,order=order,size=size)
+    plt.xlabel('Month')
+    plt.ylabel('Sales')
+    plt.title('Sales in Millions')
+
 
