b627c0d9ae9c16830d69edb2fa876b0dccf40b2e
Vagus30 <vishwas.tejaswi@gmail.com>
2018-07-31 19:53:03 +0000

    Done

--- q03_stacked_point_plot/build.py
+++ q03_stacked_point_plot/build.py
@@ -1,3 +1,4 @@
+# %load q03_stacked_point_plot/build.py
 import pandas as pd
 import numpy as np
 #import sys
@@ -8,5 +9,19 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import seaborn as sns
 
-#"write your solution here"
+#'write your solution here'
     
+def q03_stacked_point_plot(path,x_column_name='month',y_column_name='Sales',hue='year',order_of_the_axis=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec']):
+    x_train,x_valid = q02_data_splitter(path)
+    #x_train['Month'] = pd.to_datetime(df['Month'])
+    x_train['year']  = x_train['Month'].dt.year
+    x_train['month'] = x_train['Month'].dt.strftime('%b')
+    plt.figure(figsize=(16, 7))
+    sns.pointplot(x='month', y='Sales', hue='year', data=x_train, x_order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+       'Sep', 'Oct', 'Nov', 'Dec'])
+    plt.xlabel('month')
+    plt.ylabel('$ millions')
+    plt.title('Sales')
+    plt.legend(loc='upper right')
+    
+
