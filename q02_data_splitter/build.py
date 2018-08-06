ba68fb86ec60f7070bdd07757181b7782dd4b87b
HardikaB <hardi.2607@gmail.com>
2018-08-06 10:16:02 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_splitter/build.py
 import pandas as pd
 import numpy as np
 import datetime
@@ -7,3 +8,11 @@ import math
 from greyatomlib.time_series_101_project.q01_load_data.build import q01_load_data
 
 '''Write your solution here'''
+def q02_data_splitter(path):
+    tss=q01_load_data(path)
+    tss['Month']=pd.to_datetime(tss['Month'])
+    X_train = tss[tss['Month'] <datetime.datetime(1971, 10, 1, 0, 0, 0)]
+    X_valid = tss[tss['Month'] >=datetime.datetime(1971, 10, 1, 0, 0, 0)]
+    return X_train, X_valid
+
+
