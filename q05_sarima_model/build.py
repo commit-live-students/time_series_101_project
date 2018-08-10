45d0e2d420c960cac5e7a4e6029a61c438c82ba0
HardikaB <hardi.2607@gmail.com>
2018-08-10 13:26:06 +0000

    Done

--- q05_sarima_model/build.py
+++ q05_sarima_model/build.py
@@ -1,3 +1,4 @@
+# %load q05_sarima_model/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -5,10 +6,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 # import sys
 # sys.path.append('./')
+path='data/perrin-freres-monthly-champagne.csv'
 from greyatomlib.time_series_101_project.q02_data_splitter.build import q02_data_splitter
 train, validation = q02_data_splitter(path)
+import datetime
 
 
-"write your solution here"
+'write your solution here'
+def q05_sarima_model(path):
+    train, validation = q02_data_splitter(path)
+    train.index=train['Month']
+    train=train[['Sales']]
+    train.index.name=None
+
+    validation.index=validation['Month']
+    validation=validation[['Sales']]
+    validation.index.name=None
+    return train, validation
     
+    
+
 
