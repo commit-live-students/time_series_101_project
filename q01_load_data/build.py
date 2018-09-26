f446d885f44401c4ba4fcb0052f781324417c154
RohinSequeira <sequeira.rohin@gmail.com>
2018-09-26 18:33:57 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,12 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-"write your solution here"
+'write your solution here'
    
+def q01_load_data(path):
+    df = pd.read_csv(path)
+    return df
+
 
