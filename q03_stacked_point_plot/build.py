3f9761410d8208a9a9cca8929ba207678fae2813
dharmil25 <dharmil2524_shah@ymail.com>
2018-12-19 13:37:46 +0000

    Done

--- q03_stacked_point_plot/build.py
+++ q03_stacked_point_plot/build.py
@@ -1,3 +1,4 @@
+# %load q03_stacked_point_plot/build.py
 import pandas as pd
 import numpy as np
 #import sys
@@ -8,5 +9,26 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import seaborn as sns
 
-#"write your solution here"
-    
+
+path = 'data/perrin-freres-monthly-champagne.csv'
+
+#'write your solution here'
+def q03_stacked_point_plot(path,x,y,hue,x_order):
+    df = pd.read_csv(path, index_col=False)
+    df["Month"] = pd.to_datetime(df["Month"])
+    df["year"] = df["Month"].dt.year
+    df["month"] = df["Month"].dt.strftime('%b')
+    x = "month"
+    y = "Sales"
+    hue = "year"
+    x_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    ## Split the data into train and test
+    X_train = df[df["Month"] < "1971-10-01"]
+    X_valid = df[df["Month"] >="1971-10-01"]
+    data = X_train
+    plt.figure(figsize=(16, 7))
+    sns.pointplot(x=x, y=y, hue=hue, data=data, x_order=x_order)
+    plt.xlabel("Month")
+    plt.ylabel("$ millions")
+    plt.title("Perrin Freres Monthly Champagne")
+    plt.legend(loc='upper right')
