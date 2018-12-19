e3d4beaa52fcd6a2caaf1122811b25c8c4d52390
dharmil25 <dharmil2524_shah@ymail.com>
2018-12-19 14:15:33 +0000

    Done

--- q04_boxplot/build.py
+++ q04_boxplot/build.py
@@ -11,15 +11,12 @@ import datetime
 from greyatomlib.time_series_101_project.q02_data_splitter.build import q02_data_splitter
 
 #'write your solution here'
-def q04_boxplot(path, x='month', y='Sales', kind='box', order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
-       'Sep', 'Oct', 'Nov', 'Dec'], size=8):
-    'write your solution here'
+def q04_boxplot(path, x="month", y="Sales", kind="box", order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], size=8):
     train, validation = q02_data_splitter(path)
-    train['year'] = train['Month'].dt.year
-    train['month'] = train['Month'].dt.strftime('%b')
+    train["year"] = train["Month"].dt.year
+    train["month"] = train["Month"].dt.strftime('%b')
     plt.figure(figsize=(16, 7))
     sns.factorplot(x=x, y=y, data=train, kind=kind, order=order, size=size)
-    plt.xlabel('Month')
-    plt.ylabel('Sale volumn in millions')
-    plt.title('Monthly sales of champagne aggregated monthly')
-
+    plt.xlabel("Month")
+    plt.ylabel("Sale volumn in millions")
+    plt.title("Monthly sales of champagne aggregated monthly")
