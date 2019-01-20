13d159d8063afabd69b489e31be82f890190f2d9
sannidh <soniknitr@gmail.com>
2019-01-20 09:11:03 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,9 +1,19 @@
+# %load q02_data_splitter/build.py
 import pandas as pd
 import numpy as np
 import datetime
 import math
-#import sys
-#sys.path.append('./')
+import sys
+sys.path.append('./')
 from greyatomlib.time_series_101_project.q01_load_data.build import q01_load_data
+path = 'data/perrin-freres-monthly-champagne.csv'
 
-'''Write your solution here'''
+def q02_data_splitter(path):
+    df = q01_load_data(path)
+    df.Month = pd.to_datetime(df.Month)
+    x_train = pd.DataFrame(df[df.Month < datetime.datetime(1971,10,1)])
+    x_valid = pd.DataFrame(df[df.Month >= datetime.datetime(1971,10,1)])
+    return x_train, x_valid
+
+
+
