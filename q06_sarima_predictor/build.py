3fa7bd5e63be39faaee23c7c0ad9805c9bfe22a8
sannidh <soniknitr@gmail.com>
2019-01-20 12:00:25 +0000

    Done

--- q06_sarima_predictor/build.py
+++ q06_sarima_predictor/build.py
@@ -1,6 +1,6 @@
+# %load q06_sarima_predictor/build.py
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -10,7 +10,17 @@ from sklearn.metrics import mean_squared_error
 #import sys
 #sys.path.append('./')
 from greyatomlib.time_series_101_project.q05_sarima_model.build import q05_sarima_model
+path = 'data/perrin-freres-monthly-champagne.csv'
 tss, tss_valid = q05_sarima_model(path)
 
-    "write your solution here"
-    
+def q06_sarima_predictor(path):
+    model = sarimax.SARIMAX(tss.Sales, trend='n', order=(1,1,1), seasonal_order=(1,1,1,12))
+    results_SARIMAX = model.fit(disp=-1)
+    pred = pd.DataFrame(results_SARIMAX.forecast(len(tss_valid)))
+    pred.columns = ['y_pred']
+    pred.index = tss_valid.index
+    measure = math.pow(mean_squared_error(tss_valid.values, pred.values), 0.5)
+    return pred, measure
+
+
+
