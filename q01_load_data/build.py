57d1ca3efb9c4aa124f1cace061597b62d9e56f3
sannidh <soniknitr@gmail.com>
2019-01-20 08:57:53 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,9 +1,12 @@
+# %load q01_load_data/build.py
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 
-path = 'data/perrin-freres-monthly-champagne.csv
+path = 'data/perrin-freres-monthly-champagne.csv'
 
-"write your solution here"
-   
+def q01_load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+
 
